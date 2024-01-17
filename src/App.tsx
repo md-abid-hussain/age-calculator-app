5aa0b9a459033b79b375064598b82661c438b969
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-01-17 23:41:38 +0530

    refactor: replaced hardcoded color value with custom variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import buttonIcon from "./assets/images/icon-arrow.svg";
 const App = () => {
   return (
-    <main className="min-h-screen flex items-center justify-center px-4 sm:px-16 bg-[#f0f0f0]">
-      <div className="w-[345px] py-12 bg-[#ffffff] rounded-t-3xl px-6 rounded-bl-3xl rounded-br-[95px]">
+    <main className="min-h-screen flex items-center justify-center px-4 sm:px-16 bg-offWhite">
+      <div className="w-[345px] py-12 bg-white rounded-t-3xl px-6 rounded-bl-3xl rounded-br-[95px]">
         <form>
           <div className="flex gap-3">
             <div className="flex flex-col gap-2">
               <label
                 htmlFor="day"
-                className="text-xs text tracking-widest font-bold text-[#716F6F]"
+                className="text-xs text tracking-widest font-bold text-smokeGray"
               >
                 DAY
               </label>
@@ -16,13 +16,13 @@ const App = () => {
                 type="number"
                 id="day"
                 name="day"
-                className="w-[86px] border-2 border-[#f0f0f0] rounded-md p-4 text-sm focus:outline-none focus:ring-[0.5px] focus:border-[#854dff]"
+                className="w-[86px] border-2 border-offWhite rounded-md p-4 text-sm focus:outline-none focus:ring-[0.5px] focus:border-customPurple"
               />
             </div>
             <div className="flex flex-col gap-2">
               <label
                 htmlFor="month"
-                className="text-xs text tracking-widest font-bold text-[#716F6F]"
+                className="text-xs text tracking-widest font-bold text-smokeGray"
               >
                 MONTH
               </label>
@@ -30,13 +30,13 @@ const App = () => {
                 type="number"
                 id="month"
                 name="month"
-                className="w-[86px] border-2 border-[#f0f0f0] rounded-md p-4 text-sm focus:outline-none focus:ring-[0.5px] focus:border-[#854dff]"
+                className="w-[86px] border-2 border-offWhite rounded-md p-4 text-sm focus:outline-none focus:ring-[0.5px] focus:border-customPurple"
               />
             </div>
             <div className="flex flex-col gap-2">
               <label
                 htmlFor="year"
-                className="text-xs text tracking-widest font-bold text-[#716F6F]"
+                className="text-xs text tracking-widest font-bold text-smokeGray"
               >
                 YEAR
               </label>
@@ -44,14 +44,14 @@ const App = () => {
                 type="number"
                 id="year"
                 name="year"
-                className="w-[86px] border-2 border-[#f0f0f0] rounded-md p-4 text-sm focus:outline-none focus:ring-[0.5px] focus:border-[#854dff]"
+                className="w-[86px] border-2 border-offWhite rounded-md p-4 text-sm focus:outline-none focus:ring-[0.5px] focus:border-customPurple"
               />
             </div>
           </div>
-          <div className="my-8 flex before:content-[''] before:h-[2px] before:translate-y-8 before:bg-[#f0f0f0] before:flex-1 after:content-[''] after:h-[2px] after:bg-[#f0f0f0] after:flex-1 after:translate-y-8">
+          <div className="my-8 flex before:content-[''] before:h-[2px] before:translate-y-8 before:bg-offWhite before:flex-1 after:content-[''] after:h-[2px] after:bg-offWhite after:flex-1 after:translate-y-8">
             <button
               type="submit"
-              className="bg-[#854dff] mx-auto w-16 h-16 grid place-content-center rounded-full p-4 hover:bg-black"
+              className="bg-customPurple mx-auto w-16 h-16 grid place-content-center rounded-full p-4 hover:bg-offBlack"
             >
               <img src={buttonIcon} alt="button icon" />
             </button>
@@ -59,15 +59,15 @@ const App = () => {
         </form>
         <div>
           <p className="text-6xl font-customPoppins font-black italic">
-            <span className="text-[#854dff]">32 </span>
+            <span className="text-customPurple">32 </span>
             <span className="tracking-tighter">years</span>
           </p>
           <p className="text-6xl font-customPoppins font-black italic">
-            <span className="text-[#854dff]">12 </span>
+            <span className="text-customPurple">12 </span>
             <span className="tracking-tighter">months</span>
           </p>
           <p className="text-6xl font-customPoppins font-black italic">
-            <span className="text-[#854dff]">30 </span>
+            <span className="text-customPurple">30 </span>
             <span className="tracking-tighter">days</span>
           </p>
         </div>
