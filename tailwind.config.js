cbb57773c04890bc251671df19b3c6ab3ea2a807
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-01-17 23:38:52 +0530

    feat: add custom color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'customPoppins' : ['CustomPoppins', 'sans-serif']
+      },
+      colors:{
+        'customPurple':'#854dff',
+        'lightRed':'#ff5757',
+        'offWhite':'#f0f0f0',
+        'lightGray':'#dbdbdb',
+        'smokeGray':'#716f6f',
+        'offBlack':'#141414'
+      }
+    },
   },
   plugins: [],
 }
