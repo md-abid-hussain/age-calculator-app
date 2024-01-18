2cd0a8dcd623679ecd3c4d6cdef752da3e82ff9a
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-01-19 01:20:22 +0530

    refactor: change the htmlFor attribute of label

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -27,7 +27,7 @@ const CustomInput = ({
     " w-[86px] border-2 rounded-md py-4 px-3.5 text-sm focus:outline-none focus:ring-[0.5px] text-lg font-black lg:w-[160px] lg:p-[22px] text-[28px]";
   return (
     <div className="flex flex-col gap-2 relative mb-8">
-      <label htmlFor="day" className={labelClass}>
+      <label htmlFor={id} className={labelClass}>
         {labelName}
       </label>
       <input type="number" id={id} {...register(name)} className={inputClass} />
